08ab48b9603e2d2bd223fe419f5cc061be42003f
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-09-25 13:07:18 +0300

    Timer working without refresh button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const playButton = document.querySelector("#play");
 const svgPlay = playButton.querySelector("#svg-play");
 const svgPause = playButton.querySelector("#svg-pause");
+const timerEL = document.querySelector(".timer")
 
 svgPause.style.display = "none"
+
 
 
 const toggleSVG = () => {
@@ -16,8 +18,38 @@ const toggleSVG = () => {
 
 }
 
+let number = 60;
+let timerInterval;
+const runTimer = () => {
+    if (number > 0) {
+            number--;
+    timerEL.innerHTML = `00:${number < 10 ? `0` + number : number}`
+    } else {
+        number = 60;
+        timerEL.innerHTML = "01:00"
+        clearInterval(timerInterval)
+        svgPlay.style.display = "block";
+        svgPause.style.display = "none"
+        playButton.classList.toggle("btn-green")
+        
+    }
+
+
+}
+
+
 playButton.addEventListener("click", (event) => {
-    playButton.classList.toggle("btn-green")
+    playButton.classList.toggle("btn-green");
     toggleSVG();
+
+    
+    if (svgPause.style.display === "none") {
+        clearInterval(timerInterval);
+        timerInterval = null;
+    } else {
+        if (!timerInterval) {
+            timerInterval = setInterval(runTimer, 1000);
+        }
+    }
 });
 
